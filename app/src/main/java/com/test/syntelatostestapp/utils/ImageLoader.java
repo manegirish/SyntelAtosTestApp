0bdd7883ce5985cc9d1dfa9110e71f614b73c2e1
manegirish <gmane@birdzi.com>
2021-09-12 20:56:16 +0530

    - glide module set-up

---
+++ app/src/main/java/com/test/syntelatostestapp/utils/ImageLoader.java
@@ -0,0 +1,15 @@
+package com.test.syntelatostestapp.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * @author Girish D. Mane gmane@birdzi.com
+ * Created on 9/12/21
+ * Last modified on 9/12/21
+ * All rights reserved by Birdzi In
+ */
+
+@GlideModule
+public class ImageLoader extends AppGlideModule {
+}
